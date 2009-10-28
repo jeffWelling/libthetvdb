e1e3114c2b126d58c23a6fcd53137e77c4dad8d2
Jeff Welling <Jeff.Welling@Gmail.com>
2009-10-27 20:39:25 -0700

    Initial funcs, and a search command.

--- libthetvdb.rb
+++ libthetvdb.rb
@@ -15,4 +15,50 @@
     You should have received a copy of the GNU General Public License
     along with libthetvdb.  If not, see <http://www.gnu.org/licenses/>.
 =end
+autoload :XmlSimple, 'xmlsimple'
+autoload :WWW, 'mechanize'
+autoload :ERB, 'erb'   #Used for url encoding
 
+module Thetvdb
+
+  class << self
+    #readFile takes a filename, and optionally the maximum number of lines to read.
+    #
+    #returns the lines read as an array.
+    def readFile file, max_lines=0
+      counter=0
+      read_lines=[]
+      File.open(file, 'r') {|f|
+        while (line= f.gets and counter<=max_lines)
+          read_lines << line
+          counter+=1 unless max_lines==0
+        end
+      }
+      read_lines
+    end
+
+		def agent(timeout=300)
+			a = WWW::Mechanize.new
+			a.read_timeout = timeout if timeout
+			a.user_agent_alias= 'Mac Safari'
+			a   
+		end
+
+    #
+    def initMirror
+      mirrors_xml = XmlSimple.xml_in agent.get("http://www.thetvdb.com/api/#{@apikey}/mirrors.xml").body
+      mirrors_xml['Mirror'][0]['mirrorpath'][0]
+    end
+
+    def initialize
+      @apikey=readFile(File.dirname(__FILE__) +'/apikey.txt', 1).first.strip
+      @mirror=initMirror
+    end
+    attr_reader :apikey, :mirror
+  end
+  #Search Thetvdb.com for str
+  def self.search str
+    XmlSimple.xml_in( agent.get("#{@mirror}/api/GetSeries.php?seriesname=#{ERB::Util.url_encode str}").body )
+  end
+  initialize
+end
