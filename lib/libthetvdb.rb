6d3672f194d32b4ac1fbee17cc81a49383435b4d
Jeff Welling <Jeff.Welling@Gmail.com>
2010-01-27 16:30:16 -0800

    Switched to using the submodule

--- lib/libthetvdb.rb
+++ lib/libthetvdb.rb
@@ -18,41 +18,7 @@
 autoload :XmlSimple, 'xmlsimple'
 autoload :WWW, 'mechanize'
 autoload :ERB, 'erb'   #Used for url encoding
-
-#  Created by James Edward Gray II on 2006-01-21.
-#  Copyright 2006 Gray Productions. All rights reserved.
-
-# 
-# Have your class or module <tt>extend Memoizable</tt> to gain access to the 
-# #memoize method.
-# 
-module Memoizable
-  # 
-  # This method is used to replace a computationally expensive method with an
-  # equivalent method that will answer repeat calls for indentical arguments 
-  # from a _cache_.  To use, make sure the current class extends Memoizable, 
-  # then call by passing the _name_ of the method you wish to cache results for.
-  # 
-  # The _cache_ object can be any object supporting both #[] and #[]=.  The keys
-  # used for the _cache_ are an Array of the arguments the method was called 
-  # with and the values are just the returned results of the original method 
-  # call.  The default _cache_ is a simple Hash, providing in-memory storage.
-  # 
-  def memoize( name, cache = Hash.new )
-    original = "__unmemoized_#{name}__"
-
-    # 
-    # <tt>self.class</tt> is used for the top level, to modify Object, otherwise
-    # we just modify the Class or Module directly
-    # 
-    ([Class, Module].include?(self.class) ? self : self.class).class_eval do
-      alias_method original, name
-      private      original
-      define_method(name) { |*args| cache[args] ||= send(original, *args) }
-    end
-  end
-end
-#End of credits to James Edward Gray II
+autoload :Memoizable, 'memoizable/lib/memoizable.rb'
 
 module Thetvdb
 
@@ -170,7 +136,7 @@ module Thetvdb
         return []
       end
     end
-    extend Memoizable
+    include Memoizable
     memoize :search
     memoize :getAllEpisodes
   end
